73ea6f18a72363feddddc55c91bf9133cfeedd96
Dustin Schau <dustinschau@gmail.com>
2017-04-23 17:41:45 -0500

    feat: add slide presenter view (press s to view)

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const getWebpackConfig = require('object-partners-presentation');
 const path = require('path');
 
-const configFn = getWebpackConfig({
-  resolve: {
-    alias: {
-      resources: path.resolve('resources')
+const configFn = getWebpackConfig(() => {
+  return {
+    resolve: {
+      alias: {
+        resources: path.resolve('resources')
+      }
     }
-  }
+  };
 }, {
   dirname: __dirname
 });
